0d048c66b14af2744a83801fa16cfaca3f1d99ac
jessi-dev-code <jaspreet.bhagat@ensuesoft.com>
2025-11-04 16:31:53 +0530

    added cards according to the design

---
+++ app/_lib/data/industriesweserve.tsx
@@ -0,0 +1,12 @@
+import { title } from "process";
+
+const industries = [
+
+    {
+        id: 1,
+        img:"",
+        title :"Manufacturing & Production",
+        description:"From machine operators to supervisors, we provide manpower that keeps assembly lines running efficiently and safely.",
+        link:"/"
+    }
+]
