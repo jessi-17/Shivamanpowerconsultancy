c9312c040979a5e7b5ad94f1cb95862f2eae57ea
jessi <jaspreet.bhagat@ensuesoft.com>
2026-02-08 22:37:09 +0530

    comming soon added

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import React from "react";
 import Header from "../../src/components/own/Header";
-
+import aboutUsImage from "../../public/about us heroimg.png";
+import Image from "next/image";
 const page = () => {
   return (
     <>
-    <div id="mainWrapper">
-      <Header />
-      <div className="content-wrapper">
-    <section>
-    <div> services page</div>
-    </section>
-      </div>
+      <div id="mainWrapper">
+        <Header />
+        <div className="content-wrapper">
+          <div> services page</div>
+          <section className="flex flex-col md:flex-row justify-between mt-20 gap-8">
+            <h1 className="text-bland-800 font-semibold text-4xl md:text-[67px] leading-[120%] text-left">
+              Comming Soon
+            </h1>
+            <Image
+              src={aboutUsImage}
+              alt="About Us"
+              className=" h-auto  w-auto object-cover"
+            />
+          </section>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-
-export default page
-
+export default page;
