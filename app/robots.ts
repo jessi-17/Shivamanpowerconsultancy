8512b30a5091d2098feb679f4cb466782dd6a1cf
jessi <jaspreet.bhagat17@gmail.com>
2026-04-08 13:17:31 +0530

    fixes

--- app/robots.ts
+++ app/robots.ts
@@ -9,7 +9,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: [
         "/api/",
-        "/*?l=*",
         "/*?utm_*",
         "/content.php",
         "/instructors/",
